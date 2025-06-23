adac7c882cc2971f0f0efb3851fa28117d1d5444
ciscoch <77090195+ciscoch@users.noreply.github.com>
2025-06-23 09:09:47 -0500

    Add files via upload

--- ShowTrackAI_app_stub/ShowTrackAI_app_stub/ShowTrackAI_app_stub/ContentView.swift
+++ ShowTrackAI_app_stub/ShowTrackAI_app_stub/ShowTrackAI_app_stub/ContentView.swift
@@ -1,18 +1,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    @EnvironmentObject private var router: RootRouter
+    @EnvironmentObject var router: RootRouter
     
     var body: some View {
-        switch router.current {
-        case .login:
-            LoginView()
-        case .userDash:
-            UserDashboardView()
-        case .eliteDash:
-            EliteDashboardView()
-        case .buyerDash:
-            BuyerDashboardView()
+        Group {
+            switch router.current {
+            case .login:
+                LoginView()
+            case .userDash:
+                UserDashboardView()
+            case .eliteDash:
+                EliteDashboardView()
+            case .buyerDash:
+                BuyerDashboardView()
+            }
         }
     }
+}
+
+#Preview {
+    ContentView()
+        .environmentObject(RootRouter())
 }
